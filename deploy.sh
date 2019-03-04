ade2c303dc70907a3695e703d2517281cfa806d0
srinivasarao <a.srinivasarao2468@gmail.com>
2019-03-04 23:03:40 +0530

    modified deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@
 
 # register task-definition
 sed <td-tomcat.template -e "s,@VERSION@,$1,">TASKDEF.json
-aws ecs deregister-task-definition --region us-west-2 --cli-input-json file://TASKDEF.json > REGISTERED_TASKDEF.json
+aws ecs register-task-definition --region us-west-2 --cli-input-json file://TASKDEF.json > REGISTERED_TASKDEF.json
 TASKDEFINITION_ARN=$( < REGISTERED_TASKDEF.json jq .taskDefinition.taskDefinitionArn )
 
 # create or update service
 sed "s,@@TASKDEFINITION_ARN@@,$TASKDEFINITION_ARN," <service-$2-tomcat.json >SERVICEDEF.json
-aws ecs delete-service --region us-west-2 --cli-input-json file://SERVICEDEF.json | tee SERVICE.json
+aws ecs $2-service --region us-west-2 --cli-input-json file://SERVICEDEF.json | tee SERVICE.json
