d97d1f19d86f3d21bb404186713e8e54011c9628
srinivasarao <a.srinivasarao2468@gmail.com>
2019-03-04 19:11:44 +0530

    modified td-tomcat.template file

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 #Usage: CONTAINER_VERSION=docker_container_version [create|update]
 
 # register task-definition
-sed <td-tomcat.template -e "s,@VERSION@,$CONTAINER_VERSION,">TASKDEF.json
+sed <td-tomcat.template -e "s,@VERSION@,$CONTAINER_VERSION,g">TASKDEF.json
 aws ecs register-task-definition --region us-west-2 --cli-input-json file://TASKDEF.json > REGISTERED_TASKDEF.json
 TASKDEFINITION_ARN=$( < REGISTERED_TASKDEF.json jq .taskDefinition.taskDefinitionArn )
 
